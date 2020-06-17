6827101f12dabb5be12fe7e902a86c17c9320eb9
mtslck <mats.loock@lnu.se>
2020-06-17 15:53:39 +0200

    Initial commit

---
+++ src/arrayExtension.js
@@ -0,0 +1,23 @@
+/**
+ * arrayExtension module.
+ *
+ * @module src/arrayExtension
+ * @author Johan Leitet <johan.leitet@lnu.se>
+ * @author Mats Loock <mats.loock@lnu.se>
+ * @author // TODO: YOUR NAME <YOUR EMAIL>
+ * @version 1.0.0
+ */
+
+// ------------------------------------------------------------------------------
+//  Public interface
+// ------------------------------------------------------------------------------
+
+/**
+ * Returns the sum of all object's numbers.
+ *
+ * @param {{nr: number}[]} source - An array of objects to analyze.
+ * @returns {number} - The total sum of the nr property of the objects in the provided array.
+ */
+export function getSum (source) {
+  // TODO: Write your code here!
+}
